d285fd236170be2b68a9dfb2ac8e78714df49320
Navarro Mauricio <julianmn15@gmail.com>
2022-12-20 21:46:10 -0300

    ejercicio 5

---
+++ ej5.py
@@ -0,0 +1,8 @@
+def esBisiesto(anio):
+    if(anio %4 ==0 and anio %100 !=0):
+        print('El año '+str(anio)+ ' es bisiesto')
+    else:
+        print('El año '+str(anio)+ ' no es bisiesto')
+        
+anio = int(input('Ingrese un año: '))
+esBisiesto(anio)
