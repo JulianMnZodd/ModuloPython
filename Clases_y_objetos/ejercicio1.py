acdf85cd50415d679e77fdf46a5a1c7f69579e87
Navarro Mauricio <julianmn15@gmail.com>
2022-12-21 13:06:39 -0300

    ejercicio 1 de clases

---
+++ Clases_y_objetos/ejercicio1.py
@@ -0,0 +1,19 @@
+class Vehiculo:
+    color= 'rojo'
+    ruedas= 4
+    puertas= 4
+    
+class Coche(Vehiculo):
+    velocidad=150
+    cilindrada=400
+
+coche1 = Coche()
+print(coche1.velocidad)
+print(coche1.color)
+    
+    
+    
+
+
+    
+    
