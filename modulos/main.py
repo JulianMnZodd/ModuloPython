e64319cdf9493ba9adf21905039bdac7eb0adaaf
Navarro Mauricio <julianmn15@gmail.com>
2022-12-21 15:22:03 -0300

    ejercicio 1 de modulos

---
+++ modulos/main.py
@@ -0,0 +1,6 @@
+import calculadora
+
+print('Suma:',calculadora.sumar(5,5))
+print('Resta:',calculadora.restar(5,5))
+print('Multiplicacion:',calculadora.multiplicar(5,5))
+print('division:',calculadora.dividir(5,5))
