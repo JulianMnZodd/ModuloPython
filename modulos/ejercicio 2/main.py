2a1c9ef1485707c3304a50c74b178598026dc565
Navarro Mauricio <julianmn15@gmail.com>
2022-12-22 09:54:20 -0300

    modificando ejercicio

--- modulos/ejercicio 2/main.py
+++ modulos/ejercicio 2/main.py
@@ -1,14 +1,15 @@
-"""suponiendo que trabajo 7 horas desde las 0 a las 7"""
 import time
 hrlocal=time.localtime()
 
 def irAcasa():
-    print('la hora actual es:', time.strftime('%H:%M:%S',time.localtime()))
-    if(hrlocal.tm_hour>7):
+    print('la hora actual es:', time.strftime('%H:%M',time.localtime()))
+    if(hrlocal.tm_hour>19):
         print('Puede ir a casa')
     else:
-        tiempo= 7-hrlocal.tm_hour
-        print(f'le quedan',{tiempo}+'hr de trabajo')
+        h= 18-hrlocal.tm_hour
+        m=60-hrlocal.tm_min
+        tiempo=f'{h}hs y {m} minutos'
+        print(f'le quedan {tiempo} de trabajo')
         
 irAcasa()
 
